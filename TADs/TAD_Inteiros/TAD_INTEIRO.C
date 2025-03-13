b8aa26d76014fa8de9b28d49ccc2d30769409705
Joao Vitor <joao.vitor.silva61@aluno.ifce.edu.br>
2025-03-13 17:53:17 -0300

    conclusão funções

--- TADs/TAD_Inteiros/TAD_INTEIRO.C
+++ TADs/TAD_Inteiros/TAD_INTEIRO.C
@@ -5,7 +5,15 @@ Lista *lst_cria() {
     return NULL;
 }
 
-void lst_libera(Lista *l);
+void lst_libera(Lista *l) {
+    Lista* p = l;
+    while (p != NULL)
+    {   
+        Lista* t = p->prox;
+        free(p);
+        p = t;
+    }
+}
 
 Lista *lst_insere(Lista *l, int i) {
     Lista *novo = (Lista *)malloc(sizeof(Lista));
@@ -14,13 +22,53 @@ Lista *lst_insere(Lista *l, int i) {
     return novo;
 }
 
-Lista *lst_retira(Lista *l, int v);
+Lista *lst_retira(Lista *l, int v) {
+    Lista* ant = NULL;
+    Lista* p = l;
+
+    while (p != NULL && p->info != v)
+    {
+        ant = p;
+        p = p->prox;
+    }
+
+    if (p == NULL) {
+        return l;
+    }
+
+    if (ant == NULL) {
+        l = p->prox;
+    } else {
+        ant->prox = p->prox;
+    }
+
+    free(p);
+    return l;
+}
 
 int lst_vazia(Lista *l) {
     return l == NULL;
     
 }
 
-Lista *lst_busca(Lista *l, int v);
+Lista *lst_busca(Lista *l, int v) {
+    Lista *aux = l;
+    while (aux != NULL)
+    {
+        if (aux->info == v)
+        {
+            return aux;
+        }
+        aux = aux->prox;
+    }
+    return NULL;
+}
 
-void lst_imprime(Lista *l);
+void lst_imprime(Lista *l) {
+    Lista *p = l;
+    while (p != NULL)
+    {
+        printf("%d ", p->info);
+        p = p->prox;
+    }
+}
--- TADs/main.c
+++ TADs/main.c
@@ -5,11 +5,35 @@
 int main() {
     printf("Criando uma lista de inteiros!\n");
     Lista *minha_lista = lst_cria();
-    if (lst_vazia(minha_lista))
+    
+    /*if (lst_vazia(minha_lista))
     {
         printf("Minha lista esta vazia!");
+    }*/
+    for (int i = 0; i < 10; i++)
+    {
+        minha_lista = lst_insere(minha_lista, rand()%100);
     }
+
+    lst_imprime(minha_lista);
+
+    int v;
+    printf("\n\nInforme valor a ser buscado: ");
+    scanf("%d", &v);
     
+    if(lst_busca(minha_lista, v) != NULL) {
+        printf("Valor (%d) encontrado!\n", v);
+    } else {
+        printf("Valor (%d) não encontrado!\n", v);
+    };
+
+    printf("\nInforme valor a ser removido: ");
+    scanf("%d", &v);
+    minha_lista = lst_retira(minha_lista , v);
+
+    lst_imprime(minha_lista);
+
+    lst_libera(minha_lista);
     printf("\n\n");
     return 0;
 }
